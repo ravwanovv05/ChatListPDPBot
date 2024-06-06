681a6f6d79e7f5aee333b673d50873cf6f4a5d17
ravwanovv05 <samidilloravshanov14@gmail.com>
2024-06-06 14:01:10 +0500

    Added some functions

--- bot/buttons/reply_buttons/main_buttons.py
+++ bot/buttons/reply_buttons/main_buttons.py
@@ -5,7 +5,7 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 def m_buttons():
     builder = ReplyKeyboardBuilder()
     builder.add(KeyboardButton(text='Taklif/Shikoyat'))
-    builder.add(KeyboardButton(text='PDP Academy groups and bots'))
+    builder.add(KeyboardButton(text='PDP Academy gurhlari va botlari'))
 
     return builder.as_markup(resize_keyboard=True)
 
